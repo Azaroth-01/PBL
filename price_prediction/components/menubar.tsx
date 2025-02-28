6ea7ab9e3929ed0d586982f1d333fbdc58b2fd0e
Aarya <aaryamaan.106@gmail.com>
2025-02-28 15:48:09 +0530

    first commit

---
+++ price_prediction/components/menubar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default function Menubar(){
+
+    return(
+        <div className="flex flex-row justify-between items-center bg-ember p-4">
+            <div className="text-2xl font-bold text-cpurple ml-4">
+               PriceWise 
+            </div>
+
+            <div className="text-2xl font-bold text-cpurple ">
+                Need help to predict how much the property is worth??
+            </div>
+        </div>
+
+    )
+}
